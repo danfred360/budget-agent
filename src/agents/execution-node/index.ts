4fd4b874f9c974548715685ca4463d4ec9a34bb4
Dan Frederick <dannyfrederick360@gmail.com>
2024-11-24 11:45:22 -0500

    test

--- src/agents/execution-node/index.ts
+++ src/agents/execution-node/index.ts
@@ -8,5 +8,5 @@ const commandExecutor = new CommandExecutor();
 const commander = new Commander(commandExecutor);
 
 const executeAgent = new ExecutionNode(commander, logger);
-console.log("foo");
+console.log("foo bar");
 executeAgent.start();
